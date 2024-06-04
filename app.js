017755aa622772704c23d5c103d5e77a6087d5c3
nagagogulan <sgokul8792@gmail.com>
2024-06-04 19:04:04 +0530

    commit

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
+  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.8</p>');
 })
 
 app.get('/products', (req, res) => {
   res.send([
     {
-      productId: '101',
+      productId: '1',
       price: 100
     },
     {
@@ -23,3 +23,4 @@ app.listen(port, ()=> {
   console.log(`Demo app is up and listening to port: ${port}`);
 })
  
+
